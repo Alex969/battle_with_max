b04104675520efb48e81ac48986d934cef8e1f2d
Stewart-Alexander ANNANI <wxst.stewart@gmail.com>
2021-10-07 17:57:03 +0200

    created sign_in_and_play method

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,9 +1,6 @@
 feature 'Enter names for both players' do
     scenario 'submitting names'do
-        visit('/')
-        fill_in :player_1_name, with: 'Max'
-        fill_in :player_2_name, with: 'Alex'
-        click_button 'Submit'
+        sign_in_and_play
         save_and_open_page
         expect(page).to have_content('Max vs. Alex')
     end
