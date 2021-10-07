eafdbef8196ab72272c0468b59ff7868165a1df4
Max Fry <maxcfry@outlook.com>
2021-10-07 16:22:23 +0100

    Create get play

--- app.rb
+++ app.rb
@@ -2,19 +2,26 @@ require 'sinatra/base'
 require 'sinatra/reloader'
 
 class Battle < Sinatra::Base
+  enable :sessions
+
   configure :development do
     register Sinatra::Reloader
   end
 
   get '/' do
-    #'Testing infrastructure working!'
     erb :index
   end
 
-  post '/names' do
-    @player_1_name = params[:player_1_name]
-    @player_2_name = params[:player_2_name]
+  get '/play' do 
+    @player_1_name = session[:player_1_name]
+    @player_2_name = session[:player_2_name]
     erb :play
+  end
+
+  post '/names' do
+    session[:player_1_name] = params[:player_1_name]
+    session[:player_2_name] = params[:player_2_name]
+    redirect '/play'
   end
 
   run! if app_file == $0
